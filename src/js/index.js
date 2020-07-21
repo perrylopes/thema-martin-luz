2e6edff14c4c22be47dcb188f4195c8ee3a46854
Dev Goseller <development.goseller@gmail.com>
2020-07-21 18:32:54 -0300

    posts

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
+$(document).ready(function () {
+  var slick = function () {
+    if ($(window).width() <= 768) {
+      $(".js-reduzir-gastos__itens").slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+    }
+  };
 
+  (function () {
+    slick();
+  })();
+});
